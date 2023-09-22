8c618170043fc932ae28781c4fd4c4a05f435eec
Melvin C V <melrockz@gmail.com>
2023-09-22 12:48:49 +0530

    add env.sh

---
+++ env.sh
@@ -0,0 +1,17 @@
+# NGINX
+VIRTUAL_HOST=
+
+# Wordpress FPM container
+WORDPRESS_DB_HOST=
+WORDPRESS_DB_USER=
+WORDPRESS_DB_PASSWORD=
+WORDPRESS_DB_NAME=
+
+# MySQL container
+MYSQL_ROOT_PASSWORD=
+
+# ACME
+# Set a default email ID to receive Lets Encrypt notifications
+DEFAULT_EMAIL=
+# Set to true to use the Lets Encrypt Staging CA
+LETSENCRYPT_TEST=true
